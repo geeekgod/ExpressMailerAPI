36745d6fa15b5121b0484867d0b434f70021be83
Rishabh Singh <ris8645208@gmail.com>
2021-10-09 14:17:50 +0530

    chore: add middlewares for cors and to receive json and url encoding

--- app.js
+++ app.js
@@ -1,8 +1,12 @@
 const express = require("express");
-
+const cors = require("cors");
 const port = process.env.PORT || 3000;
 
 const app = express();
+
+app.use(cors());
+app.use(express.urlencoded());
+app.use(express.json());
 
 app.listen(port, () => {
   console.log(`Server hosted on http://localhost:${port}`);
